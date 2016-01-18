33d14c17f0a5d644c4072fce8806421e2ce02944
Calvin Ku <calvin.j.ku@googlemail.com>
2016-01-18 22:12:00 +0800

    changed share.php according to the c2 post.

--- myAstroid/share.php
+++ myAstroid/share.php
@@ -1,20 +1,17 @@
+<?php
+$score = $_GET["score"];
+$scorex = explode(":", $score);
+$highscore = $scorex[0];
+?>
 <html>
 <head>
-<title>myAstroid HighScore</title>
-<meta property="og:url"                content="http://savourylie.github.io/myAstroid/index.html" />
-<meta property="og:type"               content="game" />
-<!-- <meta property="og:title"              content="Calvin's Astroid Game (beta)" /> -->
-<meta property="og:description"        content="This is best earth killing game ever! Be the traitor to your home planet and don't do anything. See how astroids dramatically destroy the blue rock!" />
-<meta property="og:image"              content="http://savourylie.github.io/myAstroid/images/background-sheet0.png" />
-<meta property="fb:app_id"             content="561568013993276" />
-<meta property="og:site_name"          content="Calvin's Github Test Site" />
-<meta property="og:title" content="Highscore on zBall is ! can you beat it?"/>
-
-<!-- <meta property="og:image" content="http://weebe.nl/jellojumper/jello.png"/> -->
-<!-- <meta property="og:site_name" content="Weebe Game Studio"/> -->
-<!-- <meta property="og:description" content="My highscore at zBall is . Try it for free!"/>  -->
+<title>Weebe Jello Jumper HighScore</title>
+<meta property="og:title" content="Highscore on Jello Jumper is <?echo $highscore;?>!"/>
+<meta property="og:image" content="http://weebe.nl/jellojumper/jello.png"/>
+<meta property="og:site_name" content="Weebe Game Studio"/>
+<meta property="og:description" content="<?echo $highscore;?> is mine new highscore on Jello Jumper! How far do you think you can get? Try it for free!"/>	
 </head>
 <body>
-<meta http-equiv="refresh" content="0;URL=http://savourylie.github.io/myAstroid" />
+<meta http-equiv="refresh" content="0;URL=http://www.yoursite.com" />
 </body>
 </html>
