caa9442a62e51b39a95a8890154ed8b5b9b8f9f0
Calvin Ku <calvin.j.ku@googlemail.com>
2016-01-18 22:00:29 +0800

    changed share.php

--- myAstroid/share.php
+++ myAstroid/share.php
@@ -1,10 +1,18 @@
 <html>
 <head>
 <title>myAstroid HighScore</title>
-<meta property="og:title" content="Highscore on zBall is ! can you beat it?"/>
-<meta property="og:image" content="http://weebe.nl/jellojumper/jello.png"/>
-<meta property="og:site_name" content="Weebe Game Studio"/>
-<meta property="og:description" content="My highscore at zBall is . Try it for free!"/> 
+<meta property="og:url"                content="http://savourylie.github.io/myAstroid/index.html" />
+<meta property="og:type"               content="game" />
+<meta property="og:title"              content="Calvin's Astroid Game (beta)" />
+<meta property="og:description"        content="This is best earth killing game ever! Be the traitor to your home planet and don't do anything. See how astroids dramatically destroy the blue rock!" />
+<meta property="og:image"              content="http://savourylie.github.io/myAstroid/images/background-sheet0.png" />
+<meta property="fb:app_id"             content="561568013993276" />
+<meta property="og:site_name"          content="Calvin's Github Test Site" />
+<!-- <meta property="og:title" content="Highscore on zBall is ! can you beat it?"/> -->
+
+<!-- <meta property="og:image" content="http://weebe.nl/jellojumper/jello.png"/> -->
+<!-- <meta property="og:site_name" content="Weebe Game Studio"/> -->
+<!-- <meta property="og:description" content="My highscore at zBall is . Try it for free!"/>  -->
 </head>
 <body>
 <meta http-equiv="refresh" content="0;URL=http://savourylie.github.io/myAstroid" />
