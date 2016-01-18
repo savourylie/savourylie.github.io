6dc6ff6cd2bfacb8411de27a1630949885e73d1b
Calvin Ku <calvin.j.ku@googlemail.com>
2016-01-18 22:02:39 +0800

    changed share.php

--- myAstroid/share.php
+++ myAstroid/share.php
@@ -3,12 +3,12 @@
 <title>myAstroid HighScore</title>
 <meta property="og:url"                content="http://savourylie.github.io/myAstroid/index.html" />
 <meta property="og:type"               content="game" />
-<meta property="og:title"              content="Calvin's Astroid Game (beta)" />
+<!-- <meta property="og:title"              content="Calvin's Astroid Game (beta)" /> -->
 <meta property="og:description"        content="This is best earth killing game ever! Be the traitor to your home planet and don't do anything. See how astroids dramatically destroy the blue rock!" />
 <meta property="og:image"              content="http://savourylie.github.io/myAstroid/images/background-sheet0.png" />
 <meta property="fb:app_id"             content="561568013993276" />
 <meta property="og:site_name"          content="Calvin's Github Test Site" />
-<!-- <meta property="og:title" content="Highscore on zBall is ! can you beat it?"/> -->
+<meta property="og:title" content="Highscore on zBall is ! can you beat it?"/>
 
 <!-- <meta property="og:image" content="http://weebe.nl/jellojumper/jello.png"/> -->
 <!-- <meta property="og:site_name" content="Weebe Game Studio"/> -->
@@ -18,6 +18,3 @@
 <meta http-equiv="refresh" content="0;URL=http://savourylie.github.io/myAstroid" />
 </body>
 </html>
-<!-- Hosting24 Analytics Code -->
-<script type="text/javascript" src="http://stats.hosting24.com/count.php"></script>
-<!-- End Of Analytics Code -->
