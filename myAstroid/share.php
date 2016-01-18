8844435a5c70f7db063171dd8487c1dacf406909
Calvin Ku <calvin.j.ku@googlemail.com>
2016-01-18 21:55:09 +0800

    added share.php file

---
+++ myAstroid/share.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>myAstroid HighScore</title>
+<meta property="og:title" content="Highscore on zBall is ! can you beat it?"/>
+<meta property="og:image" content="http://weebe.nl/jellojumper/jello.png"/>
+<meta property="og:site_name" content="Weebe Game Studio"/>
+<meta property="og:description" content="My highscore at zBall is . Try it for free!"/> 
+</head>
+<body>
+<meta http-equiv="refresh" content="0;URL=http://savourylie.github.io/myAstroid" />
+</body>
+</html>
+<!-- Hosting24 Analytics Code -->
+<script type="text/javascript" src="http://stats.hosting24.com/count.php"></script>
+<!-- End Of Analytics Code -->
