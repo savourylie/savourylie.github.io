c673a77ecf3461023b970fab5cb8b539100007a8
Calvin Ku <calvin.j.ku@googlemail.com>
2016-01-25 21:13:19 +0800

    refacted facebook login;

--- facebookTest/js/facebook.js
+++ facebookTest/js/facebook.js
@@ -1,82 +1,9 @@
-// Facebook API
-window.fbAsyncInit = function() {
-    FB.init({
-        appId: '561568013993276',
-        cookie: true, // Using cookies
-        xfbml: true,
-        version: 'v2.5'
-    });
-};
-
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-function facebookInit() {
-    // Get login status OR Login to Facebook
-    FB.getLoginStatus(function(response) {
-        myViewModel.userdata.response(response);
-        console.log("Testing out the VM... " + myViewModel.userdata.response().status);
-
-        if (response.status === 'connected') {
-            console.log('All logged in.');
-            var uid = response.authResponse.userID;
-            var accessToken = response.authResponse.accessToken;
-            console.log("UserID: " + uid);
-            console.log("AccessToken: " + accessToken);
-        }
-
-        else if (response.status === 'not_authorized') {
-            console.log('User is logged in to Facebook but not to your App.');
-            console.log('Now logging in...');
-            FB.login(function(response) {
-                console.log("This is login response object: ");
-                console.log(response);
-                console.log("grantedScopes: ");
-                console.log(response.authResponse);
-            }, {
-                scope: 'public_profile',
-                return_scopes: true
-            });
-        }
-
-        else {
-            console.log('User is not logged in to anything Facebook , he/she probably hates Facebook.');
-            console.log('Now logging in...');
-            FB.login(function(response) {
-                console.log("This is login response object: ");
-                console.log(response);
-                console.log("grantedScopes: ");
-                console.log(response.authResponse);
-            }, {
-                scope: 'public_profile',
-                return_scopes: true
-            });
-        }
-    });
-}
-
-function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
-    });
-}
 // Model
 
-var UserData = function() {
+var UserData = function(response) {
     var self = this;
 
-    this.response = ko.observable();
+    this.response = ko.observable(response);
     this.userID = ko.computed(function() {
         return self.response().authResponse.userID;
     }, this);
@@ -139,10 +66,84 @@ console.log("facebook.js loaded.");
 var ViewModel = function() {
     var self = this;
 
-    this.userdata = new UserData();
+    // Facebook API
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId: '561568013993276',
+            cookie: true, // Using cookies
+            xfbml: true,
+            version: 'v2.5'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    function facebookInit() {
+
+    }
+
+    function testAPI() {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+          console.log('Successful login for: ' + response.name);
+          document.getElementById('status').innerHTML =
+            'Thanks for logging in, ' + response.name + '!';
+        });
+    }
+
+    ///////////////////////////////////////////////////////////////////
 
     this.fbLogin = function() {
-        facebookInit();
+        // Get login status OR Login to Facebook
+        FB.getLoginStatus(function(response) {
+            self.userdata = new UserData(response);
+            console.log("Testing out the VM... " + self.userdata.response().status);
+
+            if (response.status === 'connected') {
+                console.log('All logged in.');
+                var uid = response.authResponse.userID;
+                var accessToken = response.authResponse.accessToken;
+                console.log("UserID: " + uid);
+                console.log("AccessToken: " + accessToken);
+            }
+
+            else if (response.status === 'not_authorized') {
+                console.log('User is logged in to Facebook but not to your App.');
+                console.log('Now logging in...');
+                FB.login(function(response) {
+                    console.log("This is login response object: ");
+                    console.log(response);
+                    console.log("grantedScopes: ");
+                    console.log(response.authResponse);
+                }, {
+                    scope: 'public_profile',
+                    return_scopes: true
+                });
+            }
+
+            else {
+                console.log('User is not logged in to anything Facebook , he/she probably hates Facebook.');
+                console.log('Now logging in...');
+                FB.login(function(response) {
+                    console.log("This is login response object: ");
+                    console.log(response);
+                    console.log("grantedScopes: ");
+                    console.log(response.authResponse);
+                }, {
+                    scope: 'public_profile',
+                    return_scopes: true
+                });
+            }
+        });
     };
 
     this.fbOut = function() {
