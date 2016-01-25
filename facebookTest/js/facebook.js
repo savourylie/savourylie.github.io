f925b6cdcd5c069a1345d7535498bf9548392d12
Calvin Ku <calvin.j.ku@googlemail.com>
2016-01-25 21:23:43 +0800

    response all good?

--- facebookTest/js/facebook.js
+++ facebookTest/js/facebook.js
@@ -1,3 +1,35 @@
+// Facebook API
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: '561568013993276',
+        cookie: true, // Using cookies
+        xfbml: true,
+        version: 'v2.5'
+    });
+    console.log("FB API initiated.");
+};
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+}
+
+
 // Model
 
 var UserData = function(response) {
@@ -58,55 +90,16 @@ var UserData = function(response) {
 
 // ViewModel
 
-console.log("facebook.js loaded.");
-
-// Facebook
 
 
 var ViewModel = function() {
     var self = this;
 
-    // Facebook API
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId: '561568013993276',
-            cookie: true, // Using cookies
-            xfbml: true,
-            version: 'v2.5'
-        });
-    };
-
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {
-            return;
-        }
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-
-    function facebookInit() {
-
-    }
-
-    function testAPI() {
-        console.log('Welcome!  Fetching your information.... ');
-        FB.api('/me', function(response) {
-          console.log('Successful login for: ' + response.name);
-          document.getElementById('status').innerHTML =
-            'Thanks for logging in, ' + response.name + '!';
-        });
-    }
-
-    ///////////////////////////////////////////////////////////////////
-
     this.fbLogin = function() {
         // Get login status OR Login to Facebook
         FB.getLoginStatus(function(response) {
-            self.userdata = new UserData(response);
-            console.log("Testing out the VM... " + self.userdata.response().status);
+            console.log("Response all good!....... " + response);
+            // self.userdata = new UserData(response);
 
             if (response.status === 'connected') {
                 console.log('All logged in.');
